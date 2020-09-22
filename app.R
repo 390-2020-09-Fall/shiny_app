8790ea6c61ce237f7021f7437d81933c78715fb1
esscott <esscott@smith.edu>
2020-09-22 16:41:41 -0400

    Loaded data & generated static plot

--- app.R
+++ app.R
@@ -8,6 +8,36 @@
 #
 
 library(shiny)
+library(tidyverse)
+
+# Load data
+spptable <- here::here("data/scbi.spptable.csv") %>%
+  read_csv()
+stem1 <- here::here("data/scbi.stem1.csv") %>%
+  read_csv()
+stem2 <- here::here("data/scbi.stem2.csv") %>%
+  read_csv()
+stem3 <- here::here("data/scbi.stem3.csv") %>%
+  read_csv() %>%
+  filter(dbh != "NULL") %>%
+  mutate(dbh = as.numeric(dbh))
+
+# Combine all three census tables
+stem <- rbind(stem1, stem2, stem3) %>%
+  drop_na(dbh) %>%
+  mutate(
+    dbh = as.numeric(dbh),
+    CensusID = as.factor(CensusID),
+    Census = case_when(
+      CensusID == 1 ~ "2008-2010",
+      CensusID == 2 ~ "2013",
+      CensusID == 3 ~ "2018"
+    )
+  ) %>%
+  dplyr::filter(dbh > 0)
+
+# Join stem and spp tables
+stem_spp <- left_join(stem, spptable, by = "sp")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -36,12 +66,17 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        # # generate bins based on input$bins from ui.R
+        # x    <- faithful[, 2]
+        # bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        #
+        # # draw the histogram with the specified number of bins
+        # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      ggplot(stem_spp, aes(x = dbh, color = Census)) +
+        geom_freqpoly(position = "identity", binwidth = 125) +
+        xlab("Diameter at Breast Height (DBH)") +
+        ylab("Count") +
+        ggtitle("Distribution of DBH at SCBI-ForestGeo site across census years")
     })
 }
 
